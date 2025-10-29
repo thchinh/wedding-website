6b36476ab527f4dd3760eff8ff3669074d6159e9
Truong Huu Chinh <Chinh.TruongHuu@galliker.com>
2025-10-29 15:17:06 +0700

    Add helper page full

--- js/app/guest/helper.js
+++ js/app/guest/helper.js
@@ -1,8 +1,12 @@
-function generateGuestURL(mode, guestID) {
+function generateGuestURL(mode, guestID, time) {
   const baseURL = 'https://thchinh.github.io/wedding-website';
   const url = new URL(baseURL);
   url.searchParams.append('m', mode);
   url.searchParams.append('g', guestID);
+
+  if (time) {
+    url.searchParams.append('t', time);
+  }
   return url.toString();
 }
 
@@ -10,6 +14,7 @@ const copyButton = document.getElementById('copyButton');
 const generateButton = document.getElementById('generateButton');
 const inputMode = document.getElementById('inputMode');
 const inputGuestID = document.getElementById('inputGuestID');
+const inputTime = document.getElementById('inputTime');
 const result = document.getElementById('result');
 
 inputGuestID.addEventListener('keydown', function () {
@@ -32,12 +37,17 @@ copyButton.addEventListener('click', function () {
 generateButton.addEventListener('click', function () {
   const valueInputMode = inputMode.value;
   const valueInputGuestID = inputGuestID.value;
+  const valueTime = inputTime.value;
 
   if (!valueInputMode || !valueInputGuestID) {
     alert('Please enter both Mode and Guest ID.');
     return;
   }
 
-  const generatedURL = generateGuestURL(valueInputMode, valueInputGuestID);
+  const generatedURL = generateGuestURL(
+    valueInputMode,
+    valueInputGuestID,
+    valueTime
+  );
   result.innerText = generatedURL;
 });
