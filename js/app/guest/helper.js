5b2bad7e514532e915a28a1f82b2aa0f46b7fa9f
Truong Huu Chinh <Chinh.TruongHuu@galliker.com>
2025-10-29 15:13:46 +0700

    Add helper page

---
+++ js/app/guest/helper.js
@@ -0,0 +1,43 @@
+function generateGuestURL(mode, guestID) {
+  const baseURL = 'https://thchinh.github.io/wedding-website';
+  const url = new URL(baseURL);
+  url.searchParams.append('m', mode);
+  url.searchParams.append('g', guestID);
+  return url.toString();
+}
+
+const copyButton = document.getElementById('copyButton');
+const generateButton = document.getElementById('generateButton');
+const inputMode = document.getElementById('inputMode');
+const inputGuestID = document.getElementById('inputGuestID');
+const result = document.getElementById('result');
+
+inputGuestID.addEventListener('keydown', function () {
+  copyButton.innerText = 'Copy link';
+  result.innerText = '';
+});
+
+copyButton.addEventListener('click', function () {
+  const resultText = result.innerText;
+
+  if (!resultText) {
+    alert('No URL to copy. Please generate a URL first.');
+    return;
+  }
+
+  navigator.clipboard.writeText(resultText);
+  this.innerText = 'Copied!';
+});
+
+generateButton.addEventListener('click', function () {
+  const valueInputMode = inputMode.value;
+  const valueInputGuestID = inputGuestID.value;
+
+  if (!valueInputMode || !valueInputGuestID) {
+    alert('Please enter both Mode and Guest ID.');
+    return;
+  }
+
+  const generatedURL = generateGuestURL(valueInputMode, valueInputGuestID);
+  result.innerText = generatedURL;
+});
